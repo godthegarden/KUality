a4721367d191425dd410e040d8b0920233180f89
Jeong Won <jungwonshin0825@gmail.com>
2023-03-22 21:10:34 +0900

    Initial commit

---
+++ kuality-2023-1/main.c
@@ -0,0 +1,11 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
+#include <stdio.h>
+
+int main()
+{
+	int num;
+	printf("학번을 입력하세요 >> ");
+	scanf("%d", &num);
+	printf("학번: %d", num);
+}
