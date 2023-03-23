802cd1262f2a1b65d0c4fd2a1670401d85f92393
Jeong Won <jungwonshin0825@gmail.com>
2023-03-23 18:06:16 +0900

    Add week03 project

--- week03/test1.c
+++ week03/test1.c
@@ -1,6 +1,44 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
 #include <stdio.h>
 
 int main()
 {
-	printf("Hello 2023\n");
+	int n1, n2, n3;
+	char c;
+	printf("Input num : ");
+	scanf("%d %d %d", &n1, &n2, &n3);
+	printf("Input operator : ");
+	scanf("%c", &c);
+	scanf("%c", &c);
+	switch (c) {
+	case '*':
+		printf("%d\n", n1 * n2 * n3);
+		break;
+	case '+':
+		printf("%d\n", n1 * n2 * n3);
+		break;
+	case '-':
+		printf("%d\n", n1 - n2 - n3);
+		break;
+	case '/':
+		printf("%d\n", n1 / n2 / n3);
+		break;
+	}
+
+	if (c == '*') {
+		printf("%d\n", n1 * n2 * n3);
+	}
+	else if (c == '+') {
+		printf("%d\n", n1 * n2 * n3);
+	}
+	else if (c == '-') {
+		printf("%d\n", n1 - n2 - n3);
+	}
+	else if (c == '/') {
+		printf("%d\n", n1 / n2 / n3);
+	}
+	else {
+		printf("Invalid operator\n");
+	}
 }
